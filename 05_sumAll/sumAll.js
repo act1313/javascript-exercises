b7c9607a662612c4ac60f6e2c7c19f13f0f7e561
act1313 <act3313@gmail.com>
2022-07-17 14:42:30 -0400

    good

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let num = 0;
+    if (Math.sign(num1) == -1 || Math.sign(num2) == -1) {
+        return "ERROR";
+    }
+    else if (typeof(num1) == "number" && typeof(num2) == "number") {
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                num += i;
+            }
+            return num;
+        }
+        else {
+            for (let i = num2; i <= num1; i++) {
+                num += i;
+            }
+            return num;
+        }
+    }
+    else {
+        return "ERROR";
+    }   
 };
 
 // Do not edit below this line
